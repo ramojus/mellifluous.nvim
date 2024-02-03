a9467cd6b8efb26f26c8a060d1bf7653d5331008
Ramojus Lapinskas <41536253+ramojus@users.noreply.github.com>
2024-02-03 16:57:46 +0200

    feat(highlights): update indent-blankline to v3 (#14)

--- lua/mellifluous/highlights/plugins/indent_blankline.lua
+++ lua/mellifluous/highlights/plugins/indent_blankline.lua
@@ -3,9 +3,9 @@ local M = {}
 function M.set(hl, colors)
     local config = require('mellifluous.config').config
 
-    hl.set('IndentBlanklineChar', { fg = (config.is_bg_dark and colors.bg4) or colors.dark_bg2 })
-    hl.set('IndentBlanklineContextChar', { fg = colors.fg5 })
-    hl.set('IndentBlanklineSpaceChar', { link = 'Whitespace' })
+    hl.set('IblIndent', { fg = (config.is_bg_dark and colors.bg4) or colors.dark_bg2 })
+    hl.set('IblScope', { fg = colors.fg5 })
+    hl.set('IblWhitespace', { link = 'Whitespace' })
 end
 
 return M
