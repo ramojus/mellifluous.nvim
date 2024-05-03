048b2a75f7f4235be70d39f2d6d885c33a7f0de5
windowsrefund <mtf8>
2024-05-02 22:19:11 -0400

    better colors

--- lua/mellifluous/highlights/plugins/aerial.lua
+++ lua/mellifluous/highlights/plugins/aerial.lua
@@ -2,8 +2,8 @@ local M = {}
 
 function M.set(hl, colors)
   hl.set("AerialNormal", { fg = colors.fg2 })
-  hl.set("AerialLine", { fg = colors.ui_orange })
-  hl.set("AerialLineNC", { fg = colors.ui_orange })
+  hl.set("AerialLine", { fg = colors.fg, bg = colors.b4 })
+  hl.set("AerialLineNC", { fg = colors.fg2, bg = colors.bg3 })
 end
 
 return M
