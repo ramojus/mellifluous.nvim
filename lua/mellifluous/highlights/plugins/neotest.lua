77fa9d0b9205e30bf22a0f920f7cd3558f5867c8
windowsrefund <windowsrefund@users.noreply.github.com>
2024-05-25 02:53:26 -0400

    feat(highlights): add neotest support (#29)
    * Initial commit
    
    * remove dark SignColumn and refactor
    
    * latest based on feedback
    
    ---------
    
    Co-authored-by: windowsrefund <mtf8>

---
+++ lua/mellifluous/highlights/plugins/neotest.lua
@@ -0,0 +1,25 @@
+local M = {}
+
+function M.set(hl, colors)
+	local config = require("mellifluous.config").config
+
+	hl.set("NeotestAdapterName", { fg = colors.fg3 })
+	hl.set("NeotestFocused", { fg = hl.get("Normal").fg, bold = true })
+	hl.set("NeotestTarget", { style = { underline = true } })
+	hl.set("NeotestIndent", { fg = colors.fg4 })
+	hl.set("NeotestExpandMarker", { link = "NeotestIndent" })
+
+	hl.set("NeotestDir", { fg = hl.get("Directory").fg })
+	hl.set("NeotestFile", { fg = colors.fg2 })
+	hl.set("NeotestNamespace", { link = "@namesapce" })
+
+	hl.set("NeotestTest", { fg = colors.fg })
+	hl.set("NeotestRunning", { fg = hl.get("Function").fg })
+	hl.set("NeotestPassed", { link = "@markup.list.checked" })
+	hl.set("NeotestFailed", { fg = colors.ui_red })
+	hl.set("NeotestSkipped", { fg = colors.fg })
+	hl.set("NeotestUnknown", { fg = colors.fg })
+	hl.set("NeotestMarked", { bg = (config.is_bg_dark and colors.bg5) or colors.bg4 })
+end
+
+return M
