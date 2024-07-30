51181ff759b9acdaacc671edac2cdb93ec488dce
Antoine Cotten <hello@acotten.com>
2024-07-30 15:43:11 +0200

    fix(lazy): honor transparent_background settings
    If Normal has no bg color (NONE), Lazy does not dim the backdrop[1].
    Taking this into account, we define the bg color for LazyNormal as
    follows:
    
    - When transparent_background is enabled, we apply the same background
      as NormalFloat unconditionally. This will be either bg3 or NONE,
      depending on the value of floating_windows.
    - When transparent_background is not enabled, we apply bg2 (the current
      value, which accounts for Lazy's dimming).
    
    [1]: https://github.com/folke/lazy.nvim/blob/v11.14.1/lua/lazy/view/float.lua#L134-L160

--- lua/mellifluous/highlights/plugins/lazy.lua
+++ lua/mellifluous/highlights/plugins/lazy.lua
@@ -3,11 +3,15 @@ local M = {}
 function M.set(hl, colors)
     local config = require("mellifluous.config").config
     local groups = require("mellifluous.highlights.custom_groups").get(colors)
-    local bg = hl.get("NormalFloat").bg
 
-    hl.set("LazyNormal", { bg = colors.bg2 })
+    local f_bg = hl.get("NormalFloat").bg
+
+    local n_bg = hl.get("Normal").bg
+    local bg_normal = (type(n_bg) == "string" and n_bg == "NONE") and f_bg or colors.bg2
+
+    hl.set("LazyNormal", { bg = bg_normal })
     hl.set("LazyButton", groups.MenuButton)
-    hl.set("LazyButtonActive", groups.MenuButtonSelected(bg))
+    hl.set("LazyButtonActive", groups.MenuButtonSelected(f_bg))
     hl.set("LazyH1", { link = "LazyButtonActive" })
 
     hl.set("LazyProgressTodo", { fg = config.is_bg_dark and colors.fg5 or colors.dark_bg })
