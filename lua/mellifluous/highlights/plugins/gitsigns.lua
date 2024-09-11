5f905ceb94dc62d55b7a14ced7649041c521f04b
windowsrefund <mtf8>
2024-09-11 10:35:53 -0400

    dark colors

--- lua/mellifluous/highlights/plugins/gitsigns.lua
+++ lua/mellifluous/highlights/plugins/gitsigns.lua
@@ -5,9 +5,9 @@ function M.set(hl, colors)
     local config = require("mellifluous.config").config
 
     if config.is_bg_dark then
-        hl.set("GitSignsAdd", { fg = colors.green:with_lightness(40):with_saturation(60), bg = bg })
-        hl.set("GitSignsChange", { fg = colors.orange:with_lightness(55):with_saturation(80), bg = bg })
-        hl.set("GitSignsDelete", { fg = colors.red:with_lightness(45):with_saturation(60), bg = bg })
+        hl.set("GitSignsAdd", { fg = colors.green:with_lightness(55):with_saturation(60), bg = bg })
+        hl.set("GitSignsChange", { fg = colors.orange:with_lightness(55):with_saturation(60), bg = bg })
+        hl.set("GitSignsDelete", { fg = colors.red:with_lightness(45):with_saturation(70), bg = bg })
     else
         hl.set("GitSignsAdd", { fg = colors.green:with_lightness(75):with_saturation(80), bg = bg })
         hl.set("GitSignsChange", { fg = colors.orange:with_lightness(70):with_saturation(80), bg = bg })
