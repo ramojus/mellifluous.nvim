f9eb12119562897f4bc699cc421e705e5d225518
Antoine Cotten <hello@acotten.com>
2025-04-07 17:15:47 +0200

    feat(highlights): nvim-dap-ui widgets

--- lua/mellifluous/highlights/plugins/dap_ui.lua
+++ lua/mellifluous/highlights/plugins/dap_ui.lua
@@ -11,6 +11,8 @@ local function replicate_winbarnc_shade(hl, target_color)
 end
 
 function M.set(hl, colors)
+    local groups = require("mellifluous.highlights.custom_groups").get(colors)
+
     -- Controls
     local green_ctrl = { bg = hl.get("WinBar").bg, fg = replicate_winbar_shade(hl, colors.ui_green) }
     local green_ctrl_nc = { bg = hl.get("WinBarNC").bg, fg = replicate_winbarnc_shade(hl, colors.ui_green) }
@@ -36,6 +38,28 @@ function M.set(hl, colors)
     hl.set("DapUIStepBackNC", blue_ctrl_nc)
     hl.set("DapUIUnavailable", muted_ctrl)
     hl.set("DapUIUnavailableNC", muted_ctrl_nc)
+
+    -- Scopes
+    hl.set("DapUIScope", { fg = hl.get("Title").fg })
+    hl.set("DapUIType", { fg = hl.get("Type").fg })
+    hl.set("DapUIVariable", { fg = hl.get("Identifier").fg })
+    hl.set("DapUIModifiedValue", { fg = hl.get("Changed").fg, bold = true })
+    hl.set("DapUIDecoration", groups.IndentLine(colors.bg))
+
+    -- Breakpoints
+    hl.set("DapUIBreakpointsPath", { fg = colors.fg2 })
+    hl.set("DapUILineNumber", { fg = hl.get("Number").fg })
+    hl.set("DapUIBreakpointsCurrentLine", { fg = hl.get("Number").fg, bold = true })
+
+    -- Stacks
+    hl.set("DapUIThread", { fg = colors.green })
+    hl.set("DapUIStoppedThread", { fg = hl.get("Function").fg })
+    hl.set("DapUISource", { fg = colors.fg2 })
+
+    -- Watches
+    hl.set("DapUIWatchesEmpty", { fg = colors.fg3 })
+    hl.set("DapUIWatchesValue", { fg = hl.get("Function").fg })
+    hl.set("DapUIWatchesError", { fg = hl.get("Error").fg })
 end
 
 return M
